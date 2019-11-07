108e57859da0371beba27dd7c0429f7ea935d88e
dixonym <dixonym@mailbox.org>
2019-11-08 00:02:17 +0100

    implemented product filtering

--- js/products.js
+++ js/products.js
@@ -1,37 +1,77 @@
+
+let tags = {};
+let selectedTags = [];
+let products = [];
+
+
 $(async () => {
     // load products
-    let products  = await loadProducts();
+    products  = await loadProducts();
+    renderProducts(products);
     // add listeners
-    await addTagListeners();
+    addTagListeners(products);
 });
 
-let tags = [];
 
-async function addTagListeners(){
+function addTagListeners(){
     $('.tag').on('click', event => {
+        console.log(event);
         let clickedTag = $(event.currentTarget);
         clickedTag.toggleClass('active');
-        tags = $('#tagsContainer .tag').map(k => $(k).text());
-        console.log(tags);
-    })
+        selectedTags.splice(0, selectedTags.length);
+        $('#tagsContainer .tag.active').each((_, tag) => selectedTags.push($(tag).attr('data-name')));
+        let filteredProducts = products.filter(f => selectedTags.every(tag => f.tags.includes(tag)));
+        updateProducts(filteredProducts);
+    });
 }
 
 async function loadProducts() {
     let products =  [
-            {img: "https://via.placeholder.com/150", id: 1, title: "Test", tags: ["Animals"], photograph: "Unkown", price: 1000.33},
-            {img: "https://via.placeholder.com/150", id: 2, title: "Test2", tags: ["Animals", "Rom"], photograph: "BigTH", price: 7777.33},
-            {img: "https://via.placeholder.com/150", id: 3, title: "Test3", tags: ["Hiking", "Alpen"], photograph: "Unkown", price: 135.33},
-            {img: "https://via.placeholder.com/150", id: 4, title: "Test4", tags: ["China", "Country"], photograph: "BigTH", price: 5000.33},
-            {img: "https://via.placeholder.com/150", id: 5, title: "Test5", tags: ["Cars"], photograph: "Unkown", price: 100.33},
-            {img: "https://via.placeholder.com/150", id: 6, title: "Test6", tags: ["Nachaufnamen"], photograph: "BigTH", price: 2000.33},
+            {img: "https://via.placeholder.com/150", id: 1, title: "Test", tags: ["animals"], photograph: "Unkown", price: 1000.33},
+            {img: "https://via.placeholder.com/150", id: 2, title: "Test2", tags: ["animals", "rom"], photograph: "BigTH", price: 7777.33},
+            {img: "https://via.placeholder.com/150", id: 3, title: "Test3", tags: ["hiking", "alpen"], photograph: "Unkown", price: 135.33},
+            {img: "https://via.placeholder.com/150", id: 4, title: "Test4", tags: ["china", "country"], photograph: "BigTH", price: 5000.33},
+            {img: "https://via.placeholder.com/150", id: 5, title: "Test5", tags: ["cars"], photograph: "Unkown", price: 100.33},
+            {img: "https://via.placeholder.com/150", id: 6, title: "Test6", tags: ["nachaufnamen"], photograph: "BigTH", price: 2000.33},
         ];
-    renderProducts(products);
+    return products;
+}
+
+function updateProducts(products) {
+    const productContainer = $('#productsContainer');
+    productContainer.empty();
+    let currentTags = {};
+    products.forEach(product => {
+        const newProduct = createProduct(product);
+        productContainer.append(newProduct);
+        product.tags.forEach( tag => {
+            if (!(tag in currentTags)) {
+                currentTags[tag] = 0;
+            } 
+            currentTags[tag] += 1;
+        });
+    });
+    updateTags(currentTags);
+}
+
+function updateTags(currentTags) {
+    const tagsContainer = $('#tagsContainer');
+    tagsContainer.empty();
+    Object.keys(tags).forEach(tag => {
+        if (tag in currentTags) {
+            tags[tag] = currentTags[tag];
+        } else {
+            tags[tag] = 0;
+        }
+        const tagElement = createTag(tag, tags[tag], selectedTags.includes(tag));
+        tagsContainer.append(tagElement);
+    });
+    addTagListeners();
 }
 
 function renderProducts(products) {
     const productContainer = $('#productsContainer');
     productContainer.empty();
-    let tags = {};
     const tagsContainer = $('#tagsContainer');
     tagsContainer.empty();
     products.forEach(product => {
@@ -46,7 +86,7 @@ function renderProducts(products) {
     });
 
     Object.keys(tags).forEach(key => {
-        const tag = createTag(key, tags[key]);
+        const tag = createTag(key, tags[key], false);
         tagsContainer.append(tag);
     });
 }
@@ -71,11 +111,15 @@ function createProduct(data) {
     return product;
 }
 
-function createTag(name, count) {
+function createTag(name, count, active) {
     const tag = $('<a href="#" class="list-group-item tag"/>')
+    if (active) {
+        tag.addClass('active');
+    }
     const counter = $('<span class="float-right badge badge-light round"/>')
     counter.text(count)
     tag.text(name);
+    tag.attr("data-name", name)
     tag.append(counter);
     return tag;
 }

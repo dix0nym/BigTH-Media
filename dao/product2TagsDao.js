fd3d0dbdcd5dfecab37bcf4d25576fba567088fe
dix0nym <dixonym@mailbox.org>
2019-12-15 02:58:52 +0100

    added functionality count to tagdao

--- dao/product2TagsDao.js
+++ dao/product2TagsDao.js
@@ -1,5 +1,6 @@
 const helper = require("../helper.js");
 const TagDao = require("./tagDao.js");
+const ProductDao = require("./productDao.js");
 
 class Product2TagsDao {
 
@@ -26,6 +27,23 @@ class Product2TagsDao {
         return tags;
     }
 
+    loadByTagId(id) {
+        const tagDao = new TagDao(this._conn);
+        const productDao = new ProductDao(this._conn);
+        var sql = "SELECT * from Product2Tags WHERE TagID=?";
+        var statement = this._conn.prepare(sql);
+        var result = statement.all(id);
+        if (helper.isArrayEmpty(result))
+            return [];
+        result = helper.arrayObjectKeysToLower(result);
+        for (var i = 0; i < result.length; i++) {
+            result[i].product = productDao.loadById(result[i].productid);
+            delete result[i].productid;
+            delete result[i].tagid;
+        }
+        return result;
+    }
+
     loadAll() {
         var sql = "SELECT * from Product2Tags";
         var statement = this._conn.prepare(sql);
@@ -41,7 +59,7 @@ class Product2TagsDao {
 
     hasTags(id) {
         var sql = "SELECT COUNT(ID) AS cnt FROM Product2Tags WHERE ProductID=?";
-        var statement = statement.get(id);
+        var statement = this._conn.prepare(sql);
         var result = statment.get(id);
 
         if (result.cnt == 1) 
--- services/tags.js
+++ services/tags.js
@@ -24,7 +24,33 @@ serviceRouter.get("/tags/all/", (request, response) => {
         helper.log("Service Tags: Records loaded");
         response.status(200).json(helper.jsonMsgOK(result));
     } catch (ex) {
-        helper.logError("Service Tags: Error loading record by id. Exception occured: " + ex.message);
+        helper.logError("Service Tags: Error loading all records. Exception occured: " + ex.message);
+        response.status(400).json(helper.jsonMsgError(ex.message));
+    }
+});
+
+serviceRouter.get("/tags/count/:id", (request, response) => {
+    helper.log("Service Tags: Client request count of tag with id=" + request.params.id);
+    const tagsDao = new TagsDao(request.app.locals.dbConnection);
+    try {
+        var result = tagsDao.countById(request.params.id);
+        helper.log("Service Tags: Records loaded");
+        response.status(200).json(helper.jsonMsgOK(result));
+    } catch (ex) {
+        helper.logError("Service Tags: Error loading tag count by id. Exception occured: " + ex.message);
+        response.status(400).json(helper.jsonMsgError(ex.message));
+    }
+});
+
+serviceRouter.get("/tags/count/all/", (request, response) => {
+    helper.log("Service Tags: Client request count of all tags ");
+    const tagsDao = new TagsDao(request.app.locals.dbConnection);
+    try {
+        var result = tagsDao.countAll();
+        helper.log("Service Tags: Records loaded");
+        response.status(200).json(helper.jsonMsgOK(result));
+    } catch (ex) {
+        helper.logError("Service Tags: Error loading all tag counts. Exception occured: " + ex.message);
         response.status(400).json(helper.jsonMsgError(ex.message));
     }
 });
